3024eb844e68fc8177f893fd5bbb7dbd425ca848
Anna Remizova <55445666+apremizova@users.noreply.github.com>
2023-04-13 23:17:12 +0300

    solution kolmogorov-smirnov

--- solution.py
+++ solution.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 370558433 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    pval = stats.ks_2samp(x,y).pvalue
+    ans = True if pval < 0.08 else False
+    return ans # Ваш ответ, True или False
